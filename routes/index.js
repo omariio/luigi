ad3c550ae1498c92972cc9d211281259ae6922cd
Dougan Nash <michaelbetts@michaelflops-Macbook-Pro.local>
2015-07-15 14:40:27 -0700

    took out the jade engine, changed views to clients, uses html instead

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("before");
+  res.sendFile(path.resolve('./clients/index.html'));
+  console.log("after");
+
 });
 
 module.exports = router;
