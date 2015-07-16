4525d86f287922de7663f7f7a2e4a2de37dd551b
Tyler Smith <tylsmith@gmail.com>
2015-07-16 16:53:08 -0700

    IT WORKS WOO

--- routes/aggregators.js
+++ routes/aggregators.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 var path = require("path");
 
+//Code translation:
+//All requests that pass through this file (so all aggregator) are given a response with the
+//Access-Control-Allow-Origin heading
+//
+//note to self:
+//Learn why this is bad
+router.use('/', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 router.use("/people", require(path.resolve("./aggregators/people")));
 
 router.get("/", function(req, res){
