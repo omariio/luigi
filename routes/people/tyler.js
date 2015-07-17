7110f7d5fa85a87e2640a98669a1f5144db8e762
Tyler Smith <tylsmith@gmail.com>
2015-07-16 21:49:24 -0700

    aggregator emulator built (rename!)

--- routes/people/tyler.js
+++ routes/people/tyler.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var path = require("path");
 
 /* GET home page. */
+
+router.use('/posts', require(path.resolve("./lib/splitter"))("./json/tyler/posts.json"));
+
 router.get('/', function(req, res, next) {
   res.sendFile(path.resolve("./json/tyler/home.json"));
 });
